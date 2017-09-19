be566e73adf72d59d2907cc18e682f2b0d4e967c
Shagor Hasan <shagoraust@gmail.com>
2017-09-19 18:46:04 +0600

    Update game2.java

--- src/Mychess/game2.java
+++ src/Mychess/game2.java
@@ -53,21 +53,18 @@ public class game2 {
     int bishopfy[] = {1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4, -5, -6, -7, 1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4, -5, -6, -7};
     int queenfx[] = {-1, -2, -3, -4, -5, -6, -7, 1, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -2, -3, -4, -5, -6, -7, -1, -2, -3, -4, -5, -6, -7, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7};
     int queenfy[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4, -5, -6, -7, 1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4, -5, -6, -7, 1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4, -5, -6, -7};
-
+    
     game2(board ob5, game ob6) {
         ob = ob5;
         ob1 = ob6;
         ob2 = new check(ob, ob1, this);
         ob3 = new checkmate(ob2, this, ob);
     }
-
     public void edit() {
         cutplayer = 0;
         int tx = 0, ty = 0;
         flag = true;
-
         if (ob1.player == 6) {
-
             for (int j = 0; j < 4; j++) {
                 switch (ob1.col) {
                     case "white":
@@ -79,20 +76,15 @@ public class game2 {
                         ty = ob1.dy + bpawnfy[j];
                         break;
                 }
-
                 if ((tx >= 1 && tx <= 8) && (ty >= 1 && ty <= 8)) {
-
                     if ((ob1.x >= ob.grid[tx][ty].x1 && ob1.x <= ob.grid[tx][ty].x2) && (ob1.y >= ob.grid[tx][ty].y1 && ob1.y <= ob.grid[tx][ty].y2)) {
                         if (j == 3) {
                             if (ob1.col.equals("white")) {
                                 if (!(ob.wpawny >= 520 && ob.wpawny <= 600)) {
-
                                     break;
                                 }
-
                             } else {
                                 if (!(ob.wpawny >= 120 && ob.wpawny <= 200)) {
-
                                     break;
                                 }
                             }
@@ -108,25 +100,17 @@ public class game2 {
                             flag = false;
                             break;
                         }
-
                     }
                     if (j == 2 && ob.grid[tx][ty].player != 0) {
                         break;
                     }
-
                 }
-
             }
-
         } else if (ob1.player == 4) {
-
             for (int j = 0; j < 8; j++) {
-
                 tx = ob1.dx + knightfx[j];
                 ty = ob1.dy + knightfy[j];
-
                 if ((tx >= 1 && tx <= 8) && (ty >= 1 && ty <= 8)) {
-
                     if ((ob1.x >= ob.grid[tx][ty].x1 && ob1.x <= ob.grid[tx][ty].x2) && (ob1.y >= ob.grid[tx][ty].y1 && ob1.y <= ob.grid[tx][ty].y2)) {
                         if (ob.grid[tx][ty].player != 0) {
                             this.cutplayer = ob.grid[tx][ty].player;
@@ -137,15 +121,12 @@ public class game2 {
                         break;
                     }
                 }
-
             }
         } else if (ob1.player == 1) {
-
             for (int j = 0; j < 8; j++) {
                 tx = ob1.dx + kingfx[j];
                 ty = ob1.dy + kingfy[j];
                 if ((tx >= 1 && tx <= 8) && (ty >= 1 && ty <= 8)) {
-
                     if ((ob1.x >= ob.grid[tx][ty].x1 && ob1.x <= ob.grid[tx][ty].x2) && (ob1.y >= ob.grid[tx][ty].y1 && ob1.y <= ob.grid[tx][ty].y2)) {
                         if (ob.grid[tx][ty].player != 0) {
                             this.cutplayer = ob.grid[tx][ty].player;
@@ -158,17 +139,12 @@ public class game2 {
                 }
             }
         } else if (ob1.player == 5) {
-
             for (int j = 0; j < 28; j++) {
-
                 tx = ob1.dx + rookfx[j];
                 ty = ob1.dy + rookfy[j];
                 if ((tx >= 1 && tx <= 8) && (ty >= 1 && ty <= 8)) {
-
                     if (ob.grid[tx][ty].player != 0) {
-
                         if (j < 6) {
-
                             j = 6;
                         } else if (j < 13) {
                             j = 13;
@@ -187,21 +163,15 @@ public class game2 {
                         flag = false;
                         break;
                     }
-
                 }
             }
         } else if (ob1.player == 3) {
-
             for (int j = 0; j < 28; j++) {
-
                 tx = ob1.dx + bishopfx[j];
                 ty = ob1.dy + bishopfy[j];
                 if ((tx >= 1 && tx <= 8) && (ty >= 1 && ty <= 8)) {
-
                     if (ob.grid[tx][ty].player != 0) {
-
                         if (j < 6) {
-
                             j = 6;
                         } else if (j < 13) {
                             j = 13;
@@ -220,21 +190,15 @@ public class game2 {
                         flag = false;
                         break;
                     }
-
                 }
             }
         } else if (ob1.player == 2) {
-
             for (int j = 0; j < 56; j++) {
-
                 tx = ob1.dx + queenfx[j];
                 ty = ob1.dy + queenfy[j];
                 if ((tx >= 1 && tx <= 8) && (ty >= 1 && ty <= 8)) {
-
                     if (ob.grid[tx][ty].player != 0) {
-
                         if (j < 6) {
-
                             j = 6;
                         } else if (j < 13) {
                             j = 13;
@@ -243,7 +207,6 @@ public class game2 {
                         } else if (j < 27) {
                             j = 27;
                         } else if (j < 34) {
-
                             j = 34;
                         } else if (j < 41) {
                             j = 41;
@@ -262,17 +225,14 @@ public class game2 {
                         flag = false;
                         break;
                     }
-
                 }
             }
         }
         if (!flag) {
             flag1 = false;
             for (int i = 1; i <= 8; i++) {
-
                 for (int j = 1; j <= 8; j++) {
                     if (!ob.grid[i][j].col.equals(ob1.col)) {
-
                         flag1 = ob2.cm(i, j, ob.grid[i][j].col, ob.grid[i][j].player, false);
                     }
                     if (flag1) {
@@ -291,51 +251,37 @@ public class game2 {
                 }
             }
         }
-
         flag1 = false;
         if (flag) {
-
             ob.invalid = true;
             rec = false;
         } else {
-
             if (this.cutplayer != 0) {
-
                 if ("white".equals(this.cutcolour)) {
                     white[this.cutplayer]++;
                     whitecounter++;
                 } else {
                     blackcounter++;
                     black[this.cutplayer]++;
-
                 }
-
             }
             if (ob.playerone) {
-
                 if (ob.whitecountdown) {
-
                     ob.whitecount++;
                 }
             }
             if (ob.playertwo) {
-
                 if (ob.blackcountdown) {
-
                     ob.blackcount++;
-
                 }
             }
-
             if (ob.whitecount == 17) {
                 ob.draw = true;
                 ob.start = false;
-
             }
             if (ob.blackcount == 17) {
                 ob.draw = true;
                 ob.start = false;
-
             }
             if (whitecounter == 15) {
                 whitecounter = 0;
@@ -345,18 +291,13 @@ public class game2 {
                 blackcounter = 0;
                 ob.blackcountdown = true;
             }
-
             if (rec) {
                 recover();
-
             } else {
                 setcheck();
             }
-
         }
-
     }
-
     public void setcheck() {
         flag1 = false;
         for (int i = 1; i <= 8; i++) {
@@ -372,14 +313,11 @@ public class game2 {
             if (flag1) {
                 break;
             }
-
         }
         if (flag1) {
             flag1 = false;
             boolean b = false;
-
             for (int i = 1; i <= 8; i++) {
-
                 for (int j = 1; j <= 8; j++) {
                     if (ob.grid[i][j].player == 1 && !ob.grid[i][j].col.equals(ob1.col)) {
                         b = ob3.edit(i, j, ob.grid[i][j].col);
@@ -391,16 +329,13 @@ public class game2 {
                     break;
                 }
             }
-
             if (b) {
-
                 ob.checkmate = true;
                 if (ob.playerone) {
                     ob.playeronewin = true;
                 } else if (ob.playertwo) {
                     ob.playertwowin = true;
                 }
-
             } else {
                 ob.check = true;
             }
@@ -421,7 +356,6 @@ public class game2 {
             ob.ob1.t1.stop();
         }
     }
-
     public void set(int x, int y) {
         this.tempcol = ob.grid[x][y].col;
         this.tempplayer = ob.grid[x][y].player;
@@ -432,13 +366,11 @@ public class game2 {
         ob.grid[ob1.dx][ob1.dy].col = "nil";
         ob.grid[ob1.dx][ob1.dy].player = 0;
         ob.grid[ob1.dx][ob1.dy].image = ob.ig[13];
-
         if (ob1.player == 6) {
             rx = x;
             ry = y;
             if (ob1.col.equals("white")) {
                 if (x == 1) {
-
                     for (int i = 2; i <= 5; i++) {
                         if (white[i] != 0) {
                             rec = true;
@@ -456,11 +388,8 @@ public class game2 {
                     }
                 }
             }
-
         }
-
     }
-
     public void recover() {
         int add = 1;
         ob.recover = true;
@@ -517,9 +446,7 @@ public class game2 {
             }
         }
     }
-
     public void setplayer() {
-
         if (ob.RecoveredPlayer != 0) {
             ob.grid[rx][ry].player = ob.RecoveredPlayer;
             ob.grid[rx][ry].image = rg;
@@ -537,8 +464,6 @@ public class game2 {
             }
             ob.RecoveredPlayer = 0;
             this.setcheck();
-
         }
     }
-
 }
