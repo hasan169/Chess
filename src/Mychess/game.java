4d068dfa88cf8bc9d08c992de6740729b159f8f1
Shagor Hasan <shagoraust@gmail.com>
2017-09-19 18:42:41 +0600

    Update game.java

--- src/Mychess/game.java
+++ src/Mychess/game.java
@@ -23,22 +23,15 @@ public class game {
     int y;
     boolean flag = false;
     game2 ob2;
-
     game(board ob1) {
         ob2 = new game2(ob1, this);
         ob = ob1;
-
     }
-
     public void move(int counter, int x1, int y1) {
-
         if (counter == 1) {
-
             flag = false;
             for (int i = 1; i <= 8; i++) {
-
                 for (int j = 1; j <= 8; j++) {
-
                     if ((x1 >= ob.grid[i][j].x1 && x1 <= ob.grid[i][j].x2) && (y1 >= ob.grid[i][j].y1 && y1 <= ob.grid[i][j].y2)) {
                         if (ob.grid[i][j].col.equals(col)) {
                             counter = 0;
@@ -56,26 +49,20 @@ public class game {
                                 ob.playertwocounter = 0;
                             }
                             break;
-
                         }
                     }
                 }
-
                 if (flag) {
                     break;
                 }
             }
         }
         if (counter == 0) {
-
             flag = false;
             for (int i = 1; i <= 8; i++) {
-
                 for (int j = 1; j <= 8; j++) {
-
                     if ((x1 >= ob.grid[i][j].x1 && x1 <= ob.grid[i][j].x2) && (y1 >= ob.grid[i][j].y1 && y1 <= ob.grid[i][j].y2)) {
                         if (ob.grid[i][j].player == 0) {
-
                             flag = true;
                             break;
                         } else {
@@ -92,7 +79,6 @@ public class game {
                                     break;
                                 }
                             }
-
                             ob.check = false;
                             if (ob.playerone) {
                                 ob.playeronecounter = 1;
@@ -100,7 +86,6 @@ public class game {
                                 ob.playertwocounter = 1;
                             }
                             ob.wpawny = y1;
-
                             player = ob.grid[i][j].player;
                             col = ob.grid[i][j].col;
                             ig = ob.grid[i][j].image;
@@ -109,14 +94,12 @@ public class game {
                             flag = true;
                             break;
                         }
-
                     }
                 }
                 if (flag) {
                     break;
                 }
             }
-
         }
     }
 }
